7b559cc9862ae6b943552fdeebe550c0e9348be6
Matthew Hu <matthu24@gmail.com>
2018-01-17 18:56:54 -0800

    Fix comment

--- frontend/components/artist/artist_index_container.jsx
+++ frontend/components/artist/artist_index_container.jsx
@@ -8,9 +8,7 @@ const mapStateToProps = state => ({
   artists: Object.values(state.entities.artists)
 });
 
-//When this container is called in the route (when you navigate to a certain path)
-//mapDispatchToProps actually dispatches the action creator fetchAllArtists
-//the state is then changed to how you specified in the reducer 
+
 const mapDispatchToProps = dispatch => ({
   fetchAllArtists: () => dispatch(fetchAllArtists())
 });
