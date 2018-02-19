34ff30f0571c8099d32549d8a0e5b5e49f93a532
Matthew Hu <matthu24@gmail.com>
2018-02-19 11:09:57 -0800

    CSS transition demo login

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default () => (
   <div className="welcome">
 
     <div className = "slogan">Discover.  Listen.  Discuss.  A space to engage with the musical community around you.</div>
     <img className='splash-image' src= "https://s3-us-west-1.amazonaws.com/fullstackfiles/Splash3.png"/>
+    <Link className = 'splash-login' to='/guest'>Try Bandwhich</Link>
+
   </div>
 );
 
