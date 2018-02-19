d26e01c0bdee86dee0ac96c1eebe392df340bc42
Matthew Hu <matthu24@gmail.com>
2018-02-19 11:19:51 -0800

    Add music icon to splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -7,7 +7,7 @@ export default () => (
 
     <div className = "slogan">Discover.  Listen.  Discuss.  A space to engage with the musical community around you.</div>
     <img className='splash-image' src= "https://s3-us-west-1.amazonaws.com/fullstackfiles/Splash3.png"/>
-    <Link className = 'splash-login' to='/guest'>Try Bandwhich</Link>
+    <Link className = 'splash-login' to='/guest'><i className="fas fa-music"></i> &nbsp; Try Bandwhich</Link>
 
   </div>
 );
